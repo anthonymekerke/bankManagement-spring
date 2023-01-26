030238f901fce2c14c880f244c6f4c72f276655b
anthony.mekerke <anthony.mekerke@protonmail.com>
2023-01-26 13:21:10 +0100

    Setup CORS & CSRF configuration CSRF need to be check (had to be disable for now)

--- src/main/java/com/example/BankManagement/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/BankManagement/config/ProjectSecurityConfig.java
@@ -1,11 +1,18 @@
 package com.example.BankManagement.config;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -14,19 +21,25 @@ public class ProjectSecurityConfig {
     @Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        /*
-        * Default configuration of spring security
-        */
-        http.authorizeRequests().anyRequest().authenticated()
+        http
+        .cors().configurationSource(new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                config.setAllowedMethods(Collections.singletonList("*"));
+                config.setAllowCredentials(true);
+                config.setAllowedHeaders(Collections.singletonList("*"));
+                config.setExposedHeaders(Arrays.asList("Authorization"));
+                config.setMaxAge(3600L);
+                return config;
+            }   
+        }).and()
+        //.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
+        .csrf().disable()
+        .authorizeRequests().anyRequest().authenticated()
         .and().formLogin()
         .and().httpBasic();
-
-        /*
-         * disable authentication
-         */
-        // http.authorizeRequests().anyRequest().permitAll()
-        // .and().formLogin()
-        // .and().httpBasic();
 
         return http.build();
     }
