aa80fd71a3a36b940510b336cd60a207299c89f3
anthony.mekerke <anthony.mekerke@protonmail.com>
2023-01-31 11:04:49 +0100

    First Release

--- src/main/java/com/example/BankManagement/business/dto/IDTO.java
+++
@@ -1,6 +0,0 @@
-package com.example.BankManagement.business.dto;
-
-public interface IDTO {
-    int getId();
-    void setId(int id);
-}
--- src/main/java/com/example/BankManagement/business/service/IClientService.java
+++
@@ -1,8 +0,0 @@
-package com.example.BankManagement.business.service;
-
-import com.example.BankManagement.business.dto.ClientBasicDTO;
-import com.example.BankManagement.exception.BankManagementBusinessException;
-
-public interface IClientService {
-    ClientBasicDTO readClientByLogin(String client_login) throws BankManagementBusinessException;
-}
